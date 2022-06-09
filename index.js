8269ee394b4c252a62ace17cd87b3bc5b0148659
Ngaicode <mjombakevin376@gmail.com>
2022-06-10 00:39:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+"use strict";
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(function (sum, groups) {
+  return groups + sum;
+});
